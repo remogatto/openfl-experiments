d290f54dd3f2293454a8cfc72c8ef97e7d40c0b7
remogatto <andrea.fazzi@alcacoop.it>
2015-06-19 15:09:45 +0200

    Begin experimenting with sprites

--- SimpleBox2D/Source/Main.hx
+++ SimpleBox2D/Source/Main.hx
@@ -7,6 +7,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2BodyDef;
 import box2D.dynamics.B2DebugDraw;
+import box2D.dynamics.B2Fixture;
 import box2D.dynamics.B2FixtureDef;
 import box2D.dynamics.B2World;
 import openfl.display.Sprite;
@@ -16,6 +17,9 @@ import openfl.events.Event;
 import openfl.Lib;
 import openfl.utils.Timer;
 import openfl.events.TimerEvent;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.Assets;
 
 class Main extends Sprite {
 	
@@ -48,7 +52,13 @@ class Main extends Sprite {
 		createBox (winWidth/2, winHeight-40, winWidth/4, 10, false);
 		createBox (winWidth/2, winHeight-60, winWidth/6, 10, false);
 		
-		var timer = new Timer (100);
+		createCrate();
+		createFloor();
+		
+		// Create a boundary sensor
+		createBSensor();
+		
+		var timer = new Timer (2000);
 		timer.addEventListener (TimerEvent.TIMER, timer_onTimer);
 		timer.start();
 		
@@ -56,30 +66,46 @@ class Main extends Sprite {
 	}
 	
 	private function timer_onTimer (event:TimerEvent) {
-		createBox (winWidth/2, 0, 5, 5, true);
+		createBox (winWidth/2, 0, 25, 25, true);
+	}
+
+	private function createBSensor ():Void {
+		var bodyDefinition = new B2BodyDef ();
+		bodyDefinition.position.set (0, winHeight * PHYSICS_SCALE);
+		var polygon = new B2PolygonShape ();
+		polygon.setAsBox (winWidth * PHYSICS_SCALE, 10 * PHYSICS_SCALE);
+		var fixtureDefinition = new B2FixtureDef ();
+		fixtureDefinition.shape = polygon;
+
+		var body = World.createBody (bodyDefinition);
+		body.setUserData("eraseSensor");
+		var fixture = body.createFixture (fixtureDefinition);
+		fixture.setSensor(true);
 	}
 
 	private function createBox (x:Float, y:Float, width:Float, height:Float, dynamicBody:Bool):Void {
-		
+		var bitmap:Bitmap = null;
+		var bitmapData:BitmapData;
 		var bodyDefinition = new B2BodyDef ();
 		bodyDefinition.position.set (x * PHYSICS_SCALE, y * PHYSICS_SCALE);
 		
 		if (dynamicBody) {
-			
 			bodyDefinition.type = B2Body.b2_dynamicBody;
 			bodyDefinition.angularVelocity = 5.0;
-			
+			bitmapData = Assets.getBitmapData ("assets/crate.png");
+			bitmap = new Bitmap (bitmapData);
 		}
 		
 		var polygon = new B2PolygonShape ();
 		polygon.setAsBox (width * PHYSICS_SCALE, height * PHYSICS_SCALE);
-		
+
 		var fixtureDefinition = new B2FixtureDef ();
 		fixtureDefinition.shape = polygon;
+		fixtureDefinition.restitution = 0.5;	
 		
 		var body = World.createBody (bodyDefinition);
+		body.setUserData(bitmap);
 		body.createFixture (fixtureDefinition);
-		
 	}
 	
 	
@@ -89,8 +115,28 @@ class Main extends Sprite {
 	private function this_onEnterFrame (event:Event):Void {
 		
 		World.step (1 / 30, 10, 10);
+		
+		var body = World.getBodyList();
+
+		while (body != null) {
+			var sprite = body.getUserData();
+			addChild(sprite);
+			sprite.x = body.getPosition().x;
+			sprite.y = body.getPosition().y;
+			body = body.getNext();
+		}
+		var contact = World.getContactList();
+		while (contact != null) {
+			if (contact.getFixtureA().getBody().getUserData() == "eraseSensor") {
+				var bodyToBeErased = contact.getFixtureB().getBody();
+				World.destroyBody(bodyToBeErased);
+			}
+
+			contact = contact.getNext();
+		}
+
 		World.clearForces ();
-		World.drawDebugData ();
+		// World.drawDebugData ();
 		
 	}
 	
